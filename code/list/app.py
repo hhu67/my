f47b0d78b8f51296fd8f62f615e51b32e995d4e5
Stepan <vasha_pochta@example.com>
2026-04-16 22:59:42 +0300

    update

---
+++ code/list/app.py
@@ -0,0 +1,19 @@
+import sys
+
+lis=[]
+
+while True:
+    print('Введите 1, чтобы добавить продукт\n\nВведите стоп если хотите закончить')
+    pol=input('Ведите: ')
+    if pol=='1':
+        add_list=input('Введите предмет ')
+        if f'{add_list}' in lis:
+            print(f'{add_list} уже есть в списке')
+        else:
+            add=lis.append(add_list)
+    elif pol=='стоп':
+        print(f'Ваш список покупок {lis}')
+        sys.exit()
+    else:
+        print('Введите верный')
+    
