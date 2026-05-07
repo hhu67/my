71069f1150ac597314f6630d13dde9d64f3f616f
Stepan <vasha_pochta@example.com>
2026-05-07 14:06:10 +0300

    add l3.c

---
+++ c/l3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int pol1;
+    printf("First number ");
+    scanf("%d", &pol1);
+    int pol2;
+    printf("Second number ");
+    scanf("%d", &pol2);
+    int x = pol1*pol2;
+    printf("Square: %d\n", x);
+}
