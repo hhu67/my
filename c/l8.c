8607b489421ea2068f95448f355b5cc73b690b0b
Stepan <vasha_pochta@example.com>
2026-05-08 19:30:29 +0300

    update l8.c

--- c/l8.c
+++ c/l8.c
@@ -4,10 +4,9 @@
 int main() {
     char x[100] = "hello";
     x[0] = 'H';
-    x[1] = 'a';
-    int index = 2;
-    strcpy(&x[index], &x[index + 1]);
-    printf("%s\n", x);
+    x[1] = 'i';
+    strcpy(&x[2], &x[5]);
+    strcat(x, "!");
     int len = strlen(x);
-    printf("%d\n", len);
+    printf("Symbol: %d\n%s", len, x);
 }
