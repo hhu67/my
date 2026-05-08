96f48505a7c05644af25e4ddc8b9f6c236d3f357
Stepan <vasha_pochta@example.com>
2026-05-08 19:58:23 +0300

    update README.md

--- c/l8.c
+++ c/l8.c
@@ -2,11 +2,20 @@
 #include <string.h>
 
 int main() {
+    char pol[100];
+    printf("Enter word ");
+    scanf("%s", pol);
     char x[100] = "hello";
     x[0] = 'H';
     x[1] = 'i';
     strcpy(&x[2], &x[5]);
     strcat(x, "!");
-    int len = strlen(x);
-    printf("Symbol: %d\n%s", len, x);
+    if(strcmp(x, pol) == 0) {
+        printf("equal\n");
+    }
+    else {
+        printf("not equal\n");
+    }
+    int len = strlen(x), len2 = strlen(pol);
+    printf("Symbol: %d\n%s\nSymbol your word: %d", len, x, len2);
 }
