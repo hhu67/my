49e25d3c8b66da39654544622f57bcc70c3c4046
root <root@i.ii>
2026-05-11 19:09:36 +0300

    update git.sh

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@ git commit -m "$com"
 git remote add gitflic git@gitflic.ru:hhu67/proj.git
 git remote add github git@github.com:hhu67/my.git
 git remote add gitea git@gitek.duckdns.org:hhu67/my.git
-git remote add berg ssh://git@codeberg.org/hhu67/sergay.git
+git remote add berg ssh://git@codeberg.org/hhu67/my.git
 git push gitflic main
 git push github main
 git push gitea main 
