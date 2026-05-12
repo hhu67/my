e3d7f15489fbd4748fc155ef4f95604b8b52f978
Stepan <vasha_pochta@example.com>
2026-05-12 18:22:30 +0300

    update git.sh

--- git.sh
+++ git.sh
@@ -6,7 +6,9 @@ git remote add gitflic git@gitflic.ru:hhu67/proj.git
 git remote add github git@github.com:hhu67/my.git
 git remote add gitea git@gitek.duckdns.org:hhu67/my.git
 git remote add berg ssh://git@codeberg.org/hhu67/my.git
+git remote add gitea.org git@gitea.com:hhu67/my.git
 git push gitflic main
 git push github main
 git push gitea main 
 git push berg main
+git push gitea.org main
