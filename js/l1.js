4da9274da6b22ac619a72fa997d46216f685b09b
root <root@1673805.xorek.cloud>
2026-05-16 20:41:02 +0300

    create new folder js

---
+++ js/l1.js
@@ -0,0 +1,10 @@
+let x = {
+    name: "Алеша",
+    age: 25
+}
+x.city = "ДА";
+console.log(x.age);
+console.log(x.name);
+console.log(x.city);
+let al = ["яблоко", "человек", "бананы"];
+console.log(al[1]);
