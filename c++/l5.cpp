03bc2b79bb6eec0dac98850b479cd669acc5929c
root <root@1673805.xorek.cloud>
2026-05-28 22:52:55 +0300

    update demo flie l5.cpp, add new function polz for request numbers and sign

--- c++/l5.cpp
+++ c++/l5.cpp
@@ -1,37 +1,54 @@
 #include <iostream>
 #include <string>
 
-int main() {
-	int s = 0;
-	while(s <= 3) {
+int polz(void) {
+	while(1) {
 		std::string pol;
 		std::cout << "Знак" << std::endl;
 		std::cin >> pol;
 		double x1;
-		std::cout << "Первое число" << std::endl;
+    	std::cout << "Первое число" << std::endl;
 		std::cin >> x1;
 		double x2;
 		std::cout << "Второе число" << std::endl;
 		std::cin >> x2;
 		if(pol == "+") {
 			std::cout << x1 << pol << x2 << "=" << x1+x2 << std::endl;
-			s++;
 		}
 		else if(pol == "-") {
 			std::cout << x1 << pol << x2 << "=" << x1-x2 << std::endl;
-			s++;
+		}
+		else if(pol == "exit") {
+			break;
 		}
 		else if(pol == "*") {
 			std::cout << x1 << pol << x2 << "=" << x1*x2 << std::endl;
-			s++;
 		}
 		else if(pol == "/") {
 			std::cout << x1 << pol << x2 << "=" << x1/x2 << std::endl;
-			s++;
 		}
 		else {
 			std::cout << "Неверный знак" << std::endl;
+			break;
+		}
+	}
+	return 1;	
+}
+
+int main() {
+	while(1) {
+		std::cout << "1. Начать\n2. Закончить" << std::endl;
+		int pol1;
+		std::cout << "Введите число" << std::endl;
+		std::cin >> pol1;
+		if(pol1 == 1) {
+			polz();
+		}
+		else if(pol1 == 2) {
 			return 0;
+		}
+		else {
+			std::cout << "Введите верное число" << std::endl;
 		}
 	}
 	return 0;
