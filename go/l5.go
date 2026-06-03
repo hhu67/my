9e0566f90f22a1e3dba8b2ff65c36a7a4d791e69
hhu67 <root@1673805.xorek.cloud>
2026-06-03 06:39:00 +0300

    add new file go/l5.go for work env file

---
+++ go/l5.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("не")
+	}
+	api := os.Getenv("api_key")
+	fmt.Println(api)
+}
