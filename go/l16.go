72b2086f043516df9957702c042957b0288e4245
root <root@1836649.xorek.cloud>
2026-06-16 03:08:48 +0300

    bruh

--- go/l16.go
+++ go/l16.go
@@ -22,7 +22,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/VSZw5p9vwaCxQgU/", handler)
+	http.HandleFunc("/VSZw5p9vwaCxQgU/efNCQ3Vz3L2P8ZGv3VWp/ybZAUeqSMbPpcDsc7G6e/", handler)
 	err := http.ListenAndServe(":23455", nil)
 	if err != nil {
 		log.Fatalf("%v\n", err)
