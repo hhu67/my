54d617531823c4738ca15970f7eb6407178803a4
hhu67 <m@mailu.icu>
2026-06-25 16:40:34 +0300

    new file

---
+++ go/l19/l2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"time"
+	"bytes"
+	"io"
+)
+
+type UserStruct struct{
+	Age int `json:"age"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	user := UserStruct{
+		Age: 66,
+		Name: "Sergey",
+		Email: "Likhodeev.60@mail.ru"
+	}
+	JsonData, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+	client := &http.Client(
+		Timeout: 5 * time.Second,
+	)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:7777/user", bytes.NewBuffer(JsonData))
+	if err != nil {
+		panic(err)
+	}
