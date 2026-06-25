5ae2274baab78cedc96ad9ccf99896c167b08374
root <root@1836649.xorek.cloud>
2026-06-25 16:50:07 +0300

    update

--- go/l19/l2.go
+++ go/l19/l2.go
@@ -19,16 +19,27 @@ func main() {
 	user := UserStruct{
 		Age: 66,
 		Name: "Sergey",
-		Email: "Likhodeev.60@mail.ru"
+		Email: "Likhodeev.60@mail.ru",
 	}
 	JsonData, err := json.Marshal(user)
 	if err != nil {
 		panic(err)
 	}
-	client := &http.Client(
+	client := &http.Client{
 		Timeout: 5 * time.Second,
-	)
+	}
 	req, err := http.NewRequest("POST", "http://127.0.0.1:7777/user", bytes.NewBuffer(JsonData))
 	if err != nil {
 		panic(err)
 	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("status code %d\n", resp.StatusCode)
+	fmt.Println(string(body))
+}
